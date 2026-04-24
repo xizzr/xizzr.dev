7f9f128991d3f7f88de98d1e56a7edaf3d0a5e4c
xizzr <xizzr.dev@gmail.com>
2026-04-24 19:35:29 +0300

    test

--- functions/server.js
+++ functions/server.js
@@ -38,44 +38,44 @@ export async function onRequest(context) {
     return Response.json({ok:true});
   }
 
-    try {
-    const url = new URL(context.request.url);
-    const params = Object.fromEntries(url.searchParams);
+  //   try {
+  //   const url = new URL(context.request.url);
+  //   const params = Object.fromEntries(url.searchParams);
 
-    console.log("Incoming params:", params);
+  //   console.log("Incoming params:", params);
 
-    if (!params["openid.mode"]) {
-      return new Response("No OpenID data received");
-    }
+  //   if (!params["openid.mode"]) {
+  //     return new Response("No OpenID data received");
+  //   }
 
-    params["openid.mode"] = "check_authentication";
+  //   params["openid.mode"] = "check_authentication";
 
-    const body = new URLSearchParams(params);
+  //   const body = new URLSearchParams(params);
 
-    const steamRes = await fetch("https://steamcommunity.com/openid/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      body
-    });
+  //   const steamRes = await fetch("https://steamcommunity.com/openid/login", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/x-www-form-urlencoded"
+  //     },
+  //     body
+  //   });
 
-    const text = await steamRes.text();
+  //   const text = await steamRes.text();
 
-    console.log("Steam response:", text);
+  //   console.log("Steam response:", text);
 
-    if (!text.includes("is_valid:true")) {
-      return new Response("Invalid Login", { status: 401 });
-    }
+  //   if (!text.includes("is_valid:true")) {
+  //     return new Response("Invalid Login", { status: 401 });
+  //   }
 
-    const claimedId = params["openid.claimed_id"];
-    const steamId = claimedId.split("/").pop();
+  //   const claimedId = params["openid.claimed_id"];
+  //   const steamId = claimedId.split("/").pop();
 
-    return new Response(`Logged in as SteamID: ${steamId}`);
-  } catch (err) {
-    console.error("ERROR:", err);
-    return new Response("Internal Error", { status: 500 });
-  }
+  //   return new Response(`Logged in as SteamID: ${steamId}`);
+  // } catch (err) {
+  //   console.error("ERROR:", err);
+  //   return new Response("Internal Error", { status: 500 });
+  // }
 
   return Response.json({error: "Not found"}, {status: 404});
 }
